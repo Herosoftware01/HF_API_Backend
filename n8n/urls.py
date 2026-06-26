a8f89dc1663bfc38731326b55e5d59d394839a9b
Hero Developers <134581766+Herosoftware01@users.noreply.github.com>
2026-06-26 13:44:19 +0530

    html to image

--- n8n/urls.py
+++ n8n/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('ws_attendance/', views.ws_attendance, name='ws_attendance'), 
     path('liveprdn/', views.liveprdn, name='liveprdn'),
+    path("html-to-image/", views.html_to_image, name="html_to_image"),
 ]
