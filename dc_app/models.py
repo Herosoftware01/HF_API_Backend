bc8f143f6f6132ad595cf9d67fc65cf4629c9523
Nanthagopal29 <kirsh650@gmail.com>
2026-08-03 11:58:52 +0530

    accessory proccess Dc API

--- dc_app/models.py
+++ dc_app/models.py
@@ -163,6 +163,45 @@ class VueAccProdDel(models.Model):
         managed = False
         db_table = 'vue_acc_prod_del'
 
+class VueAccProcDel(models.Model):
+    rowno = models.BigIntegerField(db_column='RowNo', primary_key=True)  # Field name made lowercase.
+    name = models.CharField(db_column='Name', max_length=35, blank=True, null=True)  # Field name made lowercase.
+    altquantity = models.DecimalField(db_column='AltQuantity', max_digits=18, decimal_places=4)  # Field name made lowercase.
+    auom = models.CharField(max_length=25)
+    auomscale = models.IntegerField()
+    quantity = models.DecimalField(max_digits=18, decimal_places=4)
+    uom = models.CharField(max_length=25, blank=True, null=True)
+    uomscale = models.IntegerField(blank=True, null=True)
+    siz = models.CharField(max_length=50)
+    colour = models.CharField(max_length=50)
+    acc_grp = models.CharField(max_length=35)
+    acc_name = models.CharField(max_length=35)
+    incharge = models.CharField(max_length=1)
+    frmdpt = models.CharField(max_length=35)
+    todept = models.CharField(max_length=1)
+    supplier = models.CharField(max_length=35)
+    supadd1 = models.CharField(max_length=50)
+    supadd2 = models.CharField(max_length=50)
+    supadd3 = models.CharField(max_length=50)
+    supgst = models.CharField(max_length=66)
+    n = models.CharField(db_column='N', max_length=19, blank=True, null=True)  # Field name made lowercase.
+    duedate = models.DateTimeField(db_column='DueDate', blank=True, null=True)  # Field name made lowercase.
+    no = models.IntegerField()
+    date = models.DateTimeField(db_column='Date')  # Field name made lowercase.
+    orderno = models.CharField(db_column='OrderNo', max_length=50, blank=True, null=True)  # Field name made lowercase.
+    companyname = models.CharField(max_length=12)
+    address1 = models.CharField(db_column='Address1', max_length=50, blank=True, null=True)  # Field name made lowercase.
+    address2 = models.CharField(db_column='Address2', max_length=50, blank=True, null=True)  # Field name made lowercase.
+    address3 = models.CharField(db_column='Address3', max_length=50, blank=True, null=True)  # Field name made lowercase.
+    place = models.CharField(max_length=66, blank=True, null=True)
+    regno = models.CharField(db_column='RegNo', max_length=20, blank=True, null=True)  # Field name made lowercase.
+    phone1 = models.CharField(db_column='Phone1', max_length=50, blank=True, null=True)  # Field name made lowercase.
+    dye_clr = models.CharField(max_length=50, blank=True, null=True)
+
+    class Meta:
+      managed = False
+      db_table = 'vue_acc_proc_del'
+
 class TrsGatemodule(models.Model):
     module = models.CharField(db_column='Module', max_length=50, db_collation='SQL_Latin1_General_CP1_CI_AS')  # Field name made lowercase.
     qr_code_dtls = models.CharField(db_column='Qr_Code_Dtls', max_length=500, db_collation='SQL_Latin1_General_CP1_CI_AS')  # Field name made lowercase.
